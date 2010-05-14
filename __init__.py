f5a27594d9e868957d89857c5e7e2233a44ad669
Sebastian Thiel <byronimo@gmail.com>
2010-05-14 23:10:04 +0200

    Initial adjustment allowing animio to use the mrv distribution framework

--- __init__.py
+++ __init__.py
@@ -3,44 +3,6 @@ import os
 import sys
 import glob 
 
-#{ Configuration 
-# The configuration provided here is used by the template to learn more about 
-# your particular project. Additionally it is used during distribution
-
-
-# the minimum version of MRV that you require to work properly - usually the 
-# version you used during development, its worth testing older versions though to 
-# be more compatible 
-mrv_min_version = (1, 0, 0)		# ( major, minor, micro )
-
-# Information about the version of your tool
-#               major, minor, micro, releaselevel, serial
-version_info = (1,     0,     0,     'Preview',        0)
-
-# the name of your tool or program
-project_name = "AnimIO"
-
-# Author Name
-author = "Martin Freitag & Sebastian Thiel"
-
-# Authors Email
-author_email = 'not@given.com'
-
-# url of the project's main web presence
-url = 'http://gitorious.org/animio'
-
-# A short description of your project
-description = 'Convenient Animation Export and Import'
-
-# License Identification String
-license = "BSD License"
-
-# Additional Keyword Arguments to be passed to distutils.core.setup
-# This should include the 'classifiers' keyword which is important to pipy
-setup_kwargs = dict()
-
-#} END configuration
-
 #{ Initialization
 
 def _get_ext_path():
@@ -54,6 +16,7 @@ def _setup_ext_path():
 def _assure_mrv_is_available():
 	"""Assure we have access to mrv 
 	:raise ImportError: if mrv is not available or does not have a compatible version"""
+	import info
 	
 	# if we have non-mrv submodules, definitely add ext to the path.
 	module_dirs = glob.glob(_get_ext_path() + "/*")
@@ -62,12 +25,12 @@ def _assure_mrv_is_available():
 	# END setup externals 
 	
 	try:
-		import mrv
+		import mrv.info as mrvinfo
 	except ImportError:
 		# its not installed by default, try to use it as external
 		_setup_ext_path()
 		try:
-			import mrv
+			import mrv.info as mrvinfo
 		except ImportError:
 			raise ImportError("could not import mrv, please make sure it exists in your PYTHONPATH")
 		# END exception handling, 2nd attempt
@@ -77,10 +40,10 @@ def _assure_mrv_is_available():
 	# CHECK MRV VERSION
 	###################
 	# check the version
-	mmajor, mminor, mmicro = mrv_min_version
-	major, minor, micro = mrv.version_info[:3]
+	mmajor, mminor, mmicro = info.mrv_min_version
+	major, minor, micro = mrvinfo.version[:3]
 	if major < mmajor or minor < mminor or micro < mmicro:
-		raise EnvironmentError( "%s requires MRV version %i.%i.%i or higher, got %i.%i.%i instead" % ((project_name, ) + mrv_min_version + mrv.version_info[:3]))   
+		raise EnvironmentError( "%s requires MRV version %i.%i.%i or higher, got %i.%i.%i instead" % ((project_name, ) + info.mrv_min_version + mrvinfo.version[:3]))   
 	# END verify MRV version
 	
 
